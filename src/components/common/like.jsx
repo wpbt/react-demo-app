c58b53907abb4b23187c53cebfd5805ca597d533
bharat_thapa <admin@bharatt.com.np>
2021-02-19 15:52:51 +0545

    like component converted to stateless functional component

--- src/components/common/like.jsx
+++ src/components/common/like.jsx
@@ -1,10 +1,12 @@
-import React, { Component } from 'react';
-
-class Like extends Component {
-    render() { 
-        const { onLike, movie } = this.props;
-        return (<i onClick={() => onLike(movie)} style={{cursor: 'pointer'}} className={ movie.liked ? "fa fa-heart": "fa fa-heart-o" }></i>);
-    }
+const Like = ({onLike, movie}) => {
+    return (
+        <i 
+            onClick={() => onLike(movie)}
+            style={{cursor: 'pointer'}}
+            className={ movie.liked ? "fa fa-heart": "fa fa-heart-o" }
+        >
+        </i>
+    );
 }
  
 export default Like;
